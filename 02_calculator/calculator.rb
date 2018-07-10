036f66392e158a6ab21bbf6a3c3b642d1e0f099f
Chris <32041343+christopheleray@users.noreply.github.com>
2018-07-10 11:16:08 +0200

    last version

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,23 @@
-#write your code here
+def add(x, y)
+  return x + y
+end
+
+def subtract(x, y)
+  return x - y
+end
+
+def sum(x)
+  return x.sum
+end
+
+def multiply(x, y)
+  return x * y
+end
+
+def power(x, y)
+  return x ** y
+end
+
+def factorial(x)
+  (1..x).reduce(:*) || 1
+end
