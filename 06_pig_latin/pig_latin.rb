7bb1ad6fbcb57c01eeb73201f2ae1b3f575ba49d
Chris <32041343+christopheleray@users.noreply.github.com>
2018-07-10 11:17:47 +0200

    last version

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,1 +1,24 @@
-#write your code here
+
+def translate(str)
+
+  vowels = ["a", "e", "i", "o", "u", "y"]
+  special_1 = "sch"
+  special_2 = "qu"
+
+  str.gsub(/(\A|\s)\w+/) do |word|
+      word.strip!
+    while not vowels.include? word[0]
+      if special_1.include? word[0..2]
+        word << word[0..2]
+        word = word[3..-1]
+      elsif special_2.include? word[0..1]
+        word << word[0..1]
+        word = word[2..-1]
+      else
+        word << word[0]
+        word = word[1..-1]
+      end
+    end
+    ' ' + word + 'ay'
+  end.strip
+end
